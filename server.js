7b2d97e651b9ac49607409925708bce570f8915f
Guo Zhengjin <guo.zhengjin@gmail.com>
2021-09-25 18:40:13 +0800

    added jwt

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
@@ -5,42 +7,73 @@ const bcrypt = require('bcrypt')
 app.use(express.json())
 
 const users = []
+const posts = [
+    {
+        username: 'zj',
+        title: 'post 1'
+    },
+    {
+        username: 'Jim',
+        title: 'post 2'
+    }
+]
+
+const jwt = require('jsonwebtoken')
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
 
 app.get('/users', (req, res) => {
     res.json(users)
 })
 
 app.post('/users', async (req, res) => {
-    try{
+    try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         console.log(hashedPassword)
-        
+
         const user = {
             name: req.body.name,
             password: hashedPassword
         }
         users.push(user)
         res.status(201).send()
-    }catch{
+    } catch {
         res.status(500).send()
     }
 })
 
 app.post('/users/login', async (req, res) => {
     const user = users.find(user => user.name === req.body.name)
-    if (user === null){
+    if (user === null) {
         return res.status(400).send('Cannot find user')
     }
-    try{
-        if(await bcrypt.compare(req.body.password, user.password)){
-            res.send('Successful login')
-        }else{
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            // res.send('Successful login')
+            const jwt_user = { name: user.name }
+            const accessToken = jwt.sign(jwt_user, process.env.ACCESS_TOKEN_SECRET)
+            res.json({ accessToken: accessToken })
+        } else {
             res.send('Not allowed')
         }
     }
-    catch{
+    catch {
         res.status(500).send()
     }
 })
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
