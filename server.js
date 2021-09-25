9e38a53a7b3f3a3858048c25c981ec604f3bffc1
Guo Zhengjin <guo.zhengjin@gmail.com>
2021-09-25 16:17:42 +0800

    bcrypt encryption, hash with salt.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 app.use(express.json())
 
@@ -9,13 +10,37 @@ app.get('/users', (req, res) => {
     res.json(users)
 })
 
-app.post('/users', (req, res) => {
-    const user = {
-        name: req.body.name,
-        password: req.body.password
+app.post('/users', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPassword)
+        
+        const user = {
+            name: req.body.name,
+            password: hashedPassword
+        }
+        users.push(user)
+        res.status(201).send()
+    }catch{
+        res.status(500).send()
     }
-    users.push(user)
-    res.status(201).send()
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user === null){
+        return res.status(400).send('Cannot find user')
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send('Successful login')
+        }else{
+            res.send('Not allowed')
+        }
+    }
+    catch{
+        res.status(500).send()
+    }
 })
 
 app.listen(3000)
